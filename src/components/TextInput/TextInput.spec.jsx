f57ce96ec27f98fec7a1fded841d5b3fb199dba5
rodrigoms1718 <rodrigoms1718@gmail.com>
2022-02-28 13:11:58 -0300

    Testes com o Jest e Testing Library

---
+++ src/components/TextInput/TextInput.spec.jsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+import { TextInput } from '.';
+
+describe('<TextInput />', () => {
+    it('should have a valeu of searchValeu', () => {
+        const fn = jest.fn();
+        render(<TextInput handleChange={fn} searchValue={'testando'} />);
+
+        const input = screen.getByPlaceholderText(/type your search/i);
+        expect(input).toBeInTheDocument();
+
+        expect(input.value).toBe('testando');
+    });
+
+    it('should call handleChange function on each key pressed', () => {
+        const fn = jest.fn();
+        render(<TextInput handleChange={fn} />);
+
+        const input = screen.getByPlaceholderText(/type your search/i);
+        const value = 'o valor';
+
+        userEvent.type(input, value);
+
+        //screen.debug(input);
+        expect(input.value).toBe(value);
+        expect(fn).toHaveBeenCalledTimes(value.length);
+    });
+
+    it('should match snapshot', () => {
+        const fn = jest.fn();
+        const { container } = render(<TextInput handleChange={fn} />);
+        expect(container).toMatchSnapshot();
+    });
+});
