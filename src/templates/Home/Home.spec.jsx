5bc111f990bf7a2e37b0800d2d628d594c3fbb20
rodrigoms1718 <rodrigoms1718@gmail.com>
2022-03-21 23:21:10 -0300

    Testando a busca por posts

--- src/templates/Home/Home.spec.jsx
+++ src/templates/Home/Home.spec.jsx
@@ -4,6 +4,7 @@ import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import { Home } from '.';
+import userEvent from '@testing-library/user-event';
 
 //intercepta urls - chamadas http
 const handlers = [
@@ -77,5 +78,34 @@ describe('<Home />', () => {
 
     const button = screen.getByRole('button', { name: /load more posts/i });
     expect(button).toBeInTheDocument();
+  });
+
+  it('should search for posts', async () => {
+    render(<Home />);
+    const noMorePosts = screen.getByText('Não existem posts =(');
+
+    expect.assertions(10);
+
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+    //expect(search).toBeInTheDocument();
+
+    expect(screen.getByRole('heading', { name: 'title 1 1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title 2 2' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title 3 3' })).not.toBeInTheDocument();
+
+    userEvent.type(search, 'title 1');
+    expect(screen.getByRole('heading', { name: 'title 1 1' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title 2 2' })).not.toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title 3 3' })).not.toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'Search value: title 1' })).toBeInTheDocument();
+
+    userEvent.clear(search);
+    expect(screen.getByRole('heading', { name: 'title 1 1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title 2 2' })).toBeInTheDocument();
+
+    userEvent.type(search, 'post does not exit');
+    expect(screen.getByText('Não existem posts =(')).toBeInTheDocument();
   });
 });
